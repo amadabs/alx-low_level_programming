8b3bc5b69cdaf2339edf2e157436026e273e176b
Tammy Amakiri <amadabo57@gmail.com>
2022-09-09 18:59:43 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <studio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints base 16 numbers
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char bs;
+	char cd = '0';
+
+	while (cd <= '9')
+	{
+		putchar(cd);
+		cd++;
+	}
+	for (bs = 'a'; bs <= 'f'; bs++)
+	{
+		putchar(bs);
+	}
+	putchar('\n')
+
+return (0);
+}
