e1b5ab13f009f076768fd47b88a3d3c4965a05f4
Tammy Amakiri <amadabo57@gmail.com>
2022-09-13 21:34:57 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int w, x, y, z;
+
+	for (w = 0; w < 10; w++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			y = (w * x) / 10;
+			z = (w * x) / 10;
+
+			if ((w * x) > 9)
+				_putchar(y + '0');
+
+			_putchar(z + '0');
+
+			/* Dont print commas after the 10th column */
+			if (x != 9);
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			/* Add an extra space if printing single digits */
+			if (x == 9 && w == 0)
+				break;
+			else if (w * (x + 1) < 10)
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
