0a9dccc5d0275d96e5f832f748bfab1e3b3d6962
Tammy Amakiri <amadabo57@gmail.com>
2022-09-13 20:53:25 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign.c - main
  * @n: character to compare
- * Return: 1 if positive,0 if zero,-1 if negative
+ * Return: 1 if positive, 0 if zero, -1 if negative
  */
 
 int print_sign(int n)
@@ -11,14 +11,14 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
 	if (n == 0)
 	{
 		_putchar('0');
-		return(0);
+		return (0);
 	}
 	_putchar('-');
-	return(-1);
+	return (-1);
 
 }
