fe87e0f5912c6e9241b5090d572dc547b6b365c3
Tammy Amakiri <amadabo57@gmail.com>
2022-09-14 19:56:51 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,28 +19,28 @@ void print_times_table(int n)
 
 				if (q == 0)
 				{
-					_putchar('0');
-				}else if (c <= 9)
+					_putchar(c + '0');
+				}else if (c < 10 && q != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar('0' + (c / 100));
-				}else if (c > 99)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar('0' + (c / 100));
-					_putchar('0' + ((c / 10) % 10));
-					_putchar('0' + (c % 10));
-				}else
+					_putchar(c + '0');
+				}else if (c >= 10 && c < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar('0' + (c / 10));
-					_putchar('0' + (c % 10));
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				}else if (c >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
 				}
 			}
 			_putchar('\n');
