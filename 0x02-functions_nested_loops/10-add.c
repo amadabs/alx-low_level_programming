d818add6166f79636e88dc716c79da74328f3589
Tammy Amakiri <amadabo57@gmail.com>
2022-09-14 17:35:37 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adding two int
+ * @p: first int
+ * @q: second int
+ * Return: sum of both integers
+ */
+
+int add(int p, int q)
+{
+	return (p + q);
+}
