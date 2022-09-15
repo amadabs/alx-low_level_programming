04d87cff5c19ca18dcdfd86dab8be9e4dac1eeac
Tammy Amakiri <amadabo57@gmail.com>
2022-09-14 20:41:23 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int m, n, o, p;
+
+	n = 1;
+
+	o = 2;
+
+	for (m = 1; m <= 50; ++m)
+	{
+		if (n != 20365011074)
+		{
+			printf("%ld, ", n);
+		} else
+		{
+			printf("%ld\n", n);
+		}
+		p = n + o;
+		n = o;
+		o = p;
+	}
+
+	return (0);
+}
