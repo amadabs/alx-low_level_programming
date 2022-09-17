db247c3f0b41bb0333f7d79e594433ff9ce34545
Tammy Amakiri <amadabo57@gmail.com>
2022-09-16 20:12:25 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppetrcase character
+ * @c: the character to be checked
+ * Return: 1 if c uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
