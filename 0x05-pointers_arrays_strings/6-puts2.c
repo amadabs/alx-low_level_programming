6ec3d40c22ddb59d6211c72286e7d249cc41c1c6
Tammy Amakiri <amadabo57@gmail.com>
2022-09-19 17:37:13 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of two string
+ * @str: string to print the char from
+ */
+void puts2(char *str)
+{
+	int len, a;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++
+	}
+
+	for (a = 0; a < len; a += 2)
+	{
+		_putchar(str[a]);
+	}
+
+	_putchar('\n');
+}
