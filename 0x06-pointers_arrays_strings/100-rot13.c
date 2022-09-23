bc1f65da2c2fb72aa412a90fe6383d4d14509871
Tammy Amakiri <amadabo57@gmail.com>
2022-09-22 19:23:26 -0700

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
+char *rot13(char *s)
+{
+	int p, q;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (p = 0; s[p] != '\0'; p++)
+	{
+		for (q = 0; a[q] != '\0'; q++)
+		{
+			if (s[p] == a[q])
+			{
+				s[p] = b[q];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
